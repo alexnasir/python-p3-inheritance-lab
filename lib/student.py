bb4013dc5d8f3063e36a03843d2c68ae2f1ffb91
alexnasir <alexnasiali45@gmail.com>
2024-12-02 11:33:29 +0300

    main

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
+     def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Call the parent class's __init__ method
+        self.knowledge = []  # Initialize knowledge as an empty list
+
+
+     def learn(self, info):
+        '''Adds a string to the knowledge list.'''
+        self.knowledge.append(info)  # Append the new information to the knowledge list
     
-    def learn(self):
         pass
